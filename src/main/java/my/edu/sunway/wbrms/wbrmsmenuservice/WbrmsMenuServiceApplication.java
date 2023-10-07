268656cadee393f71b9277b5717760d39fdf0e79
vrnsky <vrnsky@protonmail.com>
2023-10-07 17:47:56 +0800

    Initial commit. With stub implementation

---
+++ src/main/java/my/edu/sunway/wbrms/wbrmsmenuservice/WbrmsMenuServiceApplication.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsmenuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbrmsMenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbrmsMenuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/sunway/wbrms/wbrmsmenuservice/WbrmsMenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsmenuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbrmsMenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
